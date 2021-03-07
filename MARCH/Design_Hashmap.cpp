51991b45a8a9d35c0e4ac9b62a1d84afdb5378c8
Bash009 <basbashar@gmail.com>
2021-03-07 23:03:55 +0530

    Design hashmap

---
+++ MARCH/Design_Hashmap.cpp
@@ -0,0 +1,25 @@
+class MyHashMap {
+public:
+    /** Initialize your data structure here. */
+    MyHashMap(): m_(1000000+1, -1) { // better
+        //m_ = vector<int> (1000000+1, -1); //ok
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        m_[key]=value;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        return m_[key];
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        m_[key]=-1;
+    }
+    
+private:
+    vector<int> m_;
+};
